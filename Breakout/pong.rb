724304a385bc0df460167693bf1770fdedda98cb
Efink <annuniel@gmail.com>
2013-03-15 09:42:19 -0500

    added commenting to breakout and different colors of bricks, as well as a win condition

--- Breakout/pong.rb
+++ Breakout/pong.rb
@@ -1,10 +1,14 @@
 require 'rubygems'
 require 'gosu'
+
+#
+#   Ball Class
+#
 class Ball
 	attr_reader :x, :y, :w, :h
 	def initialize(window)
 		@x = 200
-		@y = 200
+		@y = 240
 		@vx = 0
 		@vy = 0
 		@w = 20
@@ -35,8 +39,8 @@ class Ball
 	end
 	
 	def start
-		@vx = rand*10 - 5
-		@vy = rand*3 - 6
+		@vx = 5
+		@vy = -5
 	end
 	def stop
 		@x = 20 + rand*780
@@ -46,7 +50,10 @@ class Ball
 	end
 	
 end
-
+# ^ Ball
+#
+#   Life Class
+#
 class Life
 	def initialize(window, x, y)
 		@x = x
@@ -59,7 +66,10 @@ class Life
 	end
 
 end
-
+# ^ Life
+#
+#   Paddle Class
+#
 class Paddle
 	attr_reader :x, :y, :w, :h
 	def initialize(window)
@@ -87,15 +97,36 @@ class Paddle
 	
 	
 end
-
+# ^ Paddle
+#
+#   Brick Class
+#
 class Brick
-	attr_reader :x, :y, :w, :h
-	def initialize(window, x, y)
+	attr_reader :x, :y, :w, :h, :color
+	def initialize(window, x, y, color)
 		@x = x
 		@y = y
 		@w = 60
 		@h = 20
-		@image = Gosu::Image.new(window, "brick.png", false)
+		@color = color
+		#used for green bricks only
+		@hits = 0
+		@image = Gosu::Image.new(window, "#{@color}_brick.png", false)
+	end
+	
+	def destroy
+		if @color == "green"
+			@hits += 1
+			if @hits == 2
+				return true
+			else
+				return false
+			end
+		elsif @color == "red"
+			return true
+		elsif @color == "blue"
+			return false
+		end
 	end
 
 	def draw
@@ -103,29 +134,48 @@ class Brick
 	end
 
 end
-
-
+# ^ Brick
+#
+#   Main Class
+#
 class GameWindow < Gosu::Window
 
 	def initialize
 		super 800, 600, false
 		self.caption = ("Breakout")
+		#stuff that displays
 		@ball = Ball.new(self)
 		@paddle = Paddle.new(self)
 		@lives = []
+		colors = []
+		@bricks = []
+		@gameover = Gosu::Image.new(self, "gameover.png", false)
+		@game_win = Gosu::Image.new(self, "win.png", false)
+		@num_lives = 3
+		@win = false
+		#creates the lives to be displayed
 		(0..1).each do |d|
 			@lives.push Life.new(self, d*25+5, 5)
 		end
-		@bricks = []
+		#creates list of colors to randomly draw from
+		10.times do
+			colors.push "red"
+		end
+		3.times do
+			colors.push "green"
+		end
+		1.times do
+			colors.push "blue"
+		end
+		#creates the two rows of bricks
 		(2..6).each do |rows|
 			(0..8).each do |col|
-				@bricks.push Brick.new(self, col * 80+40, rows * 30)
+				@bricks.push Brick.new(self, col * 80+40, rows * 30, colors[rand*14])
 			end
 		end
-		@gameover = Gosu::Image.new(self, "gameover.png", false)
-		@num_lives = 3
+		
 	end
-	
+	#collision detection
 	def are_touching?(obj1, obj2)
 		if obj1.x > obj2.x - obj1.w and obj1.x < obj2.x + obj2.w and obj1.y > obj2.y - obj1.h and obj1.y < obj2.y + obj2.h
 			return true
@@ -161,14 +211,25 @@ class GameWindow < Gosu::Window
 		if button_down?(Gosu::KbEscape)
 			self.close
 		end
+		#collsion detection... bugs everyonce in a while... not sure why...
 		@bricks.each do |brick|
+			@win = true
+			#if one of these color of bricks is left then we still haven't won
+			if brick.color == "red" or brick.color == "green"
+				@win = false
+			end
 			if are_touching?(@ball, brick)
 				@ball.reverse_y
-				@bricks.delete brick
+				if brick.destroy
+					@bricks.delete brick
+				end
 			end
 		end
+		if @win
+			@ball.stop
+		end
+		
 		if @num_lives == 0 
-			
 			if button_down?(Gosu::KbSpace)
 				self.close
 			end
@@ -185,8 +246,10 @@ class GameWindow < Gosu::Window
 		@bricks.each do |brick|
 			brick.draw
 		end
-		if @num_lives == 0
+		if @num_lives == 0 
 			@gameover.draw(200, 200, 2)
+		elsif @win
+			@game_win.draw(200, 200, 2)
 		else
 			@ball.draw
 		end
